a52768a375037315d8f4e634d8f16e303a777056
Muhammad Musa <moosasaadat99@gmail.com>
2020-06-20 14:38:28 +0500

    Add test case for transition_model()

--- test_pagerank.py
+++ test_pagerank.py
@@ -3,7 +3,18 @@ import pagerank as pr
 
 
 class TestPageRank(unittest.TestCase):
-    pass
+    def test_transition_model(self):
+        corpus = {
+            "1.html": {"2.html", "3.html"},
+            "2.html": {"3.html"},
+            "3.html": {"2.html"},
+        }
+        page = "1.html"
+        damping_factor = 0.85
+
+        result = {"1.html": 0.05, "2.html": 0.475, "3.html": 0.475}
+        for page, rank in pr.transition_model(corpus, page, damping_factor).items():
+            self.assertAlmostEqual(result[page], rank)
 
 
 if __name__ == "__main__":
